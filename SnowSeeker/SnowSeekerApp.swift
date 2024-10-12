e114bead61517f3f8dbdf2cd65901d3c35e4df4b
Muhammad Naveed <m.naveed32001@gmail.com>
2024-10-12 19:21:56 +0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Naveed on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowSeekerTests/SnowSeekerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerTests.swift
+//  SnowSeekerTests
+//
+//  Created by Naveed on 12/10/2024.
+//
+
+import Testing
+@testable import SnowSeeker
+
+struct SnowSeekerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
